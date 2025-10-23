334aae6d7f958a50baebeb77d4c2bd7229642478
FebriDevelopers <Febriputratriadi33@gmail.com>
2025-10-23 20:00:59 +0700

    git commit -m "update"

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//scroll function
 const slide = document.querySelector('.dokumentasi-slide');
 
 slide.addEventListener('wheel', (e) => {
   e.preventDefault(); // cegah scroll vertikal
   slide.scrollLeft += e.deltaY; // ubah scroll ke horizontal
 });
+
+// fullscreen image
+const image = document.querySelector(".fullscreen").src;
