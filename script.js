a7fb6af6952ddff1d7dfce303d8df7e0153c8062
FebriDevelopers <Febriputratriadi33@gmail.com>
2025-10-26 23:07:00 +0700

    update

--- script.js
+++ script.js
@@ -7,4 +7,56 @@ slide.addEventListener('wheel', (e) => {
 });
 
 // fullscreen image
-const image = document.querySelector(".fullscreen").src;
+let currentIndex = 0;
+const images = [
+  "./img/dokumentasi osis/osis1.jpeg",
+  "./img/dokumentasi osis/osis2.jpeg",
+  "./img/dokumentasi osis/osis3.jpeg",
+  "./img/dokumentasi osis/osis4.jpeg",
+  "./img/dokumentasi osis/osis5.jpeg",
+  "./img/dokumentasi osis/osis6.jpeg",
+  "./img/dokumentasi osis/osis7.jpeg",
+  "./img/dokumentasi osis/osis8.jpeg",
+  "./img/dokumentasi osis/osis12.jpeg",
+  "./img/dokumentasi osis/osis13.jpeg",
+  "./img/dokumentasi osis/osis14.jpeg",
+  "./img/dokumentasi osis/osis15.jpeg",
+  "./img/dokumentasi osis/osis9.jpeg",
+  "./img/dokumentasi osis/osis10.jpeg",
+  "./img/dokumentasi osis/osis11.jpeg",
+  "./img/dokumentasi osis/osis15.jpeg",
+  "./img/dokumentasi osis/osis15.jpeg",
+  "./img/dokumentasi osis/osis15.jpeg"
+];
+
+function openImage(index) {
+  currentIndex = index - 1;
+  const container = document.querySelector(".shadow");
+  const foto = document.querySelector('.foto-temp');
+  const alamat = document.getElementById("index");
+  container.style.display = "flex";
+  foto.src = images[currentIndex];
+  alamat.textContent = `${currentIndex + 1}/${images.length}`;
+}
+
+function nextImg() {
+  currentIndex = (currentIndex + 1) % images.length;
+  openImage(currentIndex + 1);
+}
+
+function prevImg() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  openImage(currentIndex + 1);
+}
+
+function download() {
+  const link = document.createElement('a');
+  link.href = images[currentIndex];
+  link.download = `image-${currentIndex + 1}.jpeg`;
+  link.click();
+}
+
+function closeWin() {
+  const content = document.querySelector(".shadow");
+  content.style.display = "none";
+}
