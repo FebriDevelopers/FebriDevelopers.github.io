157028cce43fbf303a863960b0343190438d3c5d
FebriDevelopers <Febriputratriadi33@gmail.com>
2025-10-30 22:54:30 +0700

    update

--- Osis/osis.js
+++ Osis/osis.js
@@ -6,3 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
     sidebar.classList.toggle('open');
   });
 });
+const form = document.getElementById("form")
+
+form.addEventListener("submit", (e) =>{
+  e.preventDefault()
+  const nomor = 6289521027851
+  const text = document.getElementById('textBox').value
+  if (text === ""){
+    alert("isi terlebih dahulu!")
+    return
+  }
+  const url = `https://wa.me/${nomor}?text=${encodeURIComponent(text)}`
+  window.open(url,"_blank")
+} )
